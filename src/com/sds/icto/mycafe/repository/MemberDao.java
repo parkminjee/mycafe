c8209bc83a5a1622254565ccc2c0adcbce510fdf
student <student@M140414>
2015-05-26 10:09:51 +0900

    first commit

---
+++ src/com/sds/icto/mycafe/repository/MemberDao.java
@@ -0,0 +1,28 @@
+package com.sds.icto.mycafe.repository;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.ibatis.SqlMapClientTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.sds.icto.mysite.domain.MemberVo;
+
+
+@Repository
+public class MemberDao {
+	
+	@Autowired
+	SqlMapClientTemplate sqlMapClientTemplate;
+
+	public void insert(MemberVo vo) {
+		sqlMapClientTemplate.insert("member.join", vo);
+	}
+
+	public MemberVo getMember(MemberVo vo) {
+		MemberVo vo1 = (MemberVo)sqlMapClientTemplate.queryForObject("member.login",vo);
+		return vo1;
+	}
+}
