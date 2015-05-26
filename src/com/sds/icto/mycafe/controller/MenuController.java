9902ace65465000ba1b62642699b0ef410b75c19
student <student@M140413>
2015-05-26 14:45:25 +0900

    Menu Commit_0526/1445

--- src/com/sds/icto/mycafe/controller/MenuController.java
+++ src/com/sds/icto/mycafe/controller/MenuController.java
@@ -1,27 +1,51 @@
 package com.sds.icto.mycafe.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.sds.icto.mycafe.domain.MenuVo;
+import com.sds.icto.mycafe.service.MenuService;
 
 @Controller
 @RequestMapping("/menu")
 public class MenuController {
 	
-	@RequestMapping("/list")
-	public String index()
+	@Autowired
+	MenuService menuService;
+	
+	@RequestMapping("/index")
+	public String index(Model model)
 	{
-		
+		List<MenuVo> list = menuService.list();
+		model.addAttribute("list",list);
 		return "menu/list";
 	}
-	@RequestMapping("/insert")
-	public String insertForm()
+	
+	@RequestMapping(value="/write", method=RequestMethod.GET)
+	public String writeForm()
 	{
-		return "";
+		return "menu/write";
 	}
-	@RequestMapping("/insert")
-	public String insert()
+	
+	@RequestMapping(value="/write", method=RequestMethod.POST)
+	public String write(@ModelAttribute MenuVo vo, HttpSession session)
 	{
-		return "";
-	}
+		MenuVo menu_vo = (MenuVo)session.getAttribute("authMember");
+		vo.setNo(menu_vo.getNo());
+		vo.setName(menu_vo.getName());
+		
+		menuService.insert(vo);
+		return "redirect:/board/index";
+	}	
+	
 
 }
